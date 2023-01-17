55968dba33f687b1956ca4ee909ba10fe1d5f261
Andreas Jansson <andreas.jansson234@gmail.com>
2023-01-17 14:36:33 +0100

    Add class 'Tree'
    accepts an array and will have a root attribute.

--- lib/balanced_bst.rb
+++ lib/balanced_bst.rb
@@ -8,3 +8,10 @@ class Node
     @right = right
   end
 end
+
+class Tree
+  def initialize(arr)
+    @array = arr # note to self: array can be unsorted.
+    @root = # will hold value of build_tree method which has not been built yet
+  end
+end
