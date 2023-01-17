3bdef5220b291d080aad6059a7873bd3d1704313
Andreas Jansson <andreas.jansson234@gmail.com>
2023-01-17 14:33:04 +0100

    Add class Node
    class Node will include Comparable module

---
+++ lib/balanced_bst.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Node
+  include Comparable
+  def initialize(data = nil, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+end
