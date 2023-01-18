c10dd59a5120ba4c07dec75b1948fdba5b84f59f
Andreas Jansson <andreas.jansson234@gmail.com>
2023-01-18 14:35:41 +0100

    Add methods #build_tree, #pretty_print
    method #build_tree takes an array as argument and returns balanced
    binary search tree. Each node in the tree is an instance of class 'Node'
    
    method #pretty_print prints out a visual representation of the binary
    search tree constructed by #build_tree.

--- lib/balanced_bst.rb
+++ lib/balanced_bst.rb
@@ -2,6 +2,8 @@
 
 class Node
   include Comparable
+  attr_accessor :data, :left, :right
+
   def initialize(data = nil, left = nil, right = nil)
     @data = data
     @left = left
@@ -10,8 +12,32 @@ class Node
 end
 
 class Tree
+  attr_accessor :root, :array
+
   def initialize(arr)
-    @array = arr # note to self: array can be unsorted.
-    @root = # will hold value of build_tree method which has not been built yet
+    @array = arr.sort.uniq
+    @root = build_tree
+  end
+
+  def build_tree(arr = @array)
+    return nil if arr.nil? || arr.empty?
+    return Node.new(arr[0]) if arr.length == 1 # return leaf node
+
+    midpoint = arr[(arr.length - 1) / 2]
+    left_tree = build_tree(arr[0...(arr.index(midpoint))])
+    right_tree = build_tree(arr[arr.index(midpoint) + 1..arr.length])
+
+    Node.new(midpoint, left_tree, right_tree) # constructing binary search tree.
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+# example code
+arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+tree = Tree.new(arr)
+tree.pretty_print
